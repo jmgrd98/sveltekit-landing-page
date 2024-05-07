a79a811dba6dc9430dac5f1375e14991a929cc3e
joaodantasdev <jmgrd98@gmail.com>
2024-05-07 13:44:05 -0300

    product and product card components

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -22,11 +22,12 @@
             {#each [0,1,2,3,4] as index}
                 <div class="grid place-items-center relative">
                     <i class="fa-solid fa-star opacity-0"></i>
-                    <div class={'absolute top-0 left-0 grid place-items-center' + (index === 4 ? 'w-[60%] overflow-hidden' : '')}>
+                    <div class={'absolute top-0 left-0 grid place-items-center' + (index == 4 ? 'w-[60%] overflow-hidden' : '')}>
                         <i class="fa-solid fa-star text-amber-400"></i>
                     </div>
                 </div>
             {/each}
+            <p>500+</p>
         </div>
     </div>
 </SectionWrapper>
